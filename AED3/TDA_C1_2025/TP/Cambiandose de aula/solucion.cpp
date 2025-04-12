a9cb2e2f974b594e482c3e14bb3259f99c745283
valentinamoscato <valentina.moscato@appdirect.com>
2025-04-12 17:54:14 -0300

    Actualizo biblio TDA

--- AED3/TDA_C1_2025/TP/Cambiandose de aula/solucion.cpp
+++ AED3/TDA_C1_2025/TP/Cambiandose de aula/solucion.cpp
@@ -1,67 +1,88 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
-string resultado = "NO";
-int caminoMinimo = INT_MAX;
-unordered_map<tuple<int, int, int>, bool> memo;
+// i, j es la posicion del mapa
+// matriz es el mapa
+// sumaDeTemperatura es la suma de temperaturas acumulada para el recorrido
+// memo es la matriz para memoizacion
+bool esPosible(int i, int j, vector<vector<int> >& matriz, int sumaDeTemperatura, vector<vector<unordered_map<int, bool> > >& memo) {
+    bool fueraDelMapa = i < 0 || i >= matriz.size() || j < 0 || j >= matriz[0].size();
 
-int temperatura(int i, int j, vector<vector<int> >& matriz, int camino, int sumaDeTemperatura) {
-    if (i < 0 || i >= matriz.size() || j < 0 || j >= matriz[0].size()) {
-        return INT_MAX; // Fuera de los limites del mapa, retorno un numero grande para indicar que no es un camino valido
-    }
-
-    if (camino >= caminoMinimo) {
-        return INT_MAX; // Si el camino ya es mayor al camino minimo, retorno un numero grande
+    if (fueraDelMapa) {
+        return false;
     }
     
-    if (i == matriz.size() - 1 && j == matriz[0].size() - 1) {
-        if (sumaDeTemperatura + matriz[i][j] == 0) {
-            resultado = "YES"; // Encontre un camino minimo que mantiene la temperatura en 0
-            caminoMinimo = min(caminoMinimo, camino); // Actualizo el camino minimo
-        }
-        return camino; // Termine de recorrer el mapa
+    // Uso find y end porque guardo las sumas de temperaturas en un unordered_map
+    bool estaMemoizado = memo[i][j].find(sumaDeTemperatura) != memo[i][j].end();
+    if (estaMemoizado) {
+        return memo[i][j][sumaDeTemperatura]; // Retorno el resultado memoizado ya calculado
     }
 
-    // Busco el camino mas chico entre moverme hacia abajo o hacia la derecha
-    int caminoHaciaAbajo = temperatura(i + 1, j, matriz, camino + 1, sumaDeTemperatura + matriz[i][j]);
-    int caminoHaciaLaDerecha = temperatura(i, j + 1, matriz, camino + 1, sumaDeTemperatura + matriz[i][j]);
+    bool esFinalDelMapa = i == matriz.size() - 1 && j == matriz[0].size() - 1;
+    if (esFinalDelMapa) {
+        int sumaFinal = sumaDeTemperatura + matriz[i][j];
+        bool resultado = (sumaFinal == 0);
+        memo[i][j][sumaDeTemperatura] = resultado;
+        return resultado;
+    }
 
-    int nuevoCamino = min(caminoHaciaAbajo, caminoHaciaLaDerecha);
+    int pasosRestantes = (matriz.size() - 1 - i) + (matriz[0].size() - 1 - j);
 
-    return nuevoCamino;
+    int sumaConTemperaturaDeCeldaActual = sumaDeTemperatura + matriz[i][j];
+
+    // Poda: La suma actual es muy grande o muy chica, no es posible y retorno false
+    int maxSumaPosible = sumaConTemperaturaDeCeldaActual + pasosRestantes; // Si son todos 1
+    int minSumaPosible = sumaConTemperaturaDeCeldaActual - pasosRestantes; // Si son todos -1
+    
+    if (maxSumaPosible < 0 || minSumaPosible > 0) {
+        memo[i][j][sumaDeTemperatura] = false;
+        return false;
+    }
+
+    // Movimientos hacia abajo y derecha (no contemplo izquierda o arriba porque el camino debe ser minimo)
+    bool resultado = esPosible(i + 1, j, matriz, sumaConTemperaturaDeCeldaActual, memo) || 
+                    esPosible(i, j + 1, matriz, sumaConTemperaturaDeCeldaActual, memo);
+
+    memo[i][j][sumaDeTemperatura] = resultado;
+
+    return resultado;
 }
 
 int main() {
     int casosDePrueba;
     cin >> casosDePrueba;
 
-    vector<vector<vector<int> > > mapas;
-    
-    while (casosDePrueba > 0) {
+    while (casosDePrueba--) {
         int n, m;
         cin >> n >> m;
 
         vector<vector<int> > matriz(n, vector<int>(m));
-
-        int i, j;
-        // Inicializar la matriz con valores del input
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < m; j++) {
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 cin >> matriz[i][j];
             }
         }
 
-        mapas.push_back(matriz);
-        casosDePrueba--;
-    }
+        // Poda: Si la suma de las dimensiones es impar, no es posible y directamente retorno NO
+        if ((n + m - 1) % 2 != 0) {
+            cout << "NO" << endl;
+            continue;
+        }
 
-    for (int k = 0; k < mapas.size(); k++) {
-        vector<vector<int> > m = mapas[k];
-        caminoMinimo = INT_MAX; // Reinicio el camino minimo para cada caso de prueba
-        temperatura(0, 0, m, 0, 0);
-        cout << resultado << endl;
-        resultado = "NO"; // Reinicio el resultado para el siguiente caso de prueba
+        // Inicializo la matriz para memoización
+        // La matriz sigue la misma estructura que la matriz del mapa pero guarda un unordered_map para cada celda
+        // con la suma de temperatura acumulada como key y un bool que representa si es posible llegar al final
+        // con esa suma de temperatura
+        vector<vector<unordered_map<int, bool> > > memo(n, vector<unordered_map<int, bool> >(m));
+        
+        if (esPosible(0, 0, matriz, 0, memo)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
 
     return 0;
