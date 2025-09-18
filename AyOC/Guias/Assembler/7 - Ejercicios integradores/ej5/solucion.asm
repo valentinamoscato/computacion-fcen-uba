86c6b773e916075929adb3395388ef229de07837
Valentina Moscato <46605650+valentinamoscato@users.noreply.github.com>
2025-09-18 11:17:35 -0300

    Update solucion.asm

--- AyOC/Guias/Assembler/7 - Ejercicios integradores/ej5/solucion.asm
+++ AyOC/Guias/Assembler/7 - Ejercicios integradores/ej5/solucion.asm
@@ -20,45 +20,45 @@ extern strcmp
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-carta.en_juego EQU NO_COMPLETADO
-carta.nombre   EQU NO_COMPLETADO
-carta.vida     EQU NO_COMPLETADO
-carta.jugador  EQU NO_COMPLETADO
-carta.SIZE     EQU NO_COMPLETADO
+carta.en_juego EQU 0
+carta.nombre   EQU 1
+carta.vida     EQU 14
+carta.jugador  EQU 16
+carta.SIZE     EQU 18
 
-tablero.mano_jugador_rojo EQU NO_COMPLETADO
-tablero.mano_jugador_azul EQU NO_COMPLETADO
-tablero.campo             EQU NO_COMPLETADO
-tablero.SIZE              EQU NO_COMPLETADO
+tablero.mano_jugador_rojo EQU 0
+tablero.mano_jugador_azul EQU 8
+tablero.campo             EQU 16
+tablero.SIZE              EQU 416
 
-accion.invocar   EQU NO_COMPLETADO
-accion.destino   EQU NO_COMPLETADO
-accion.siguiente EQU NO_COMPLETADO
-accion.SIZE      EQU NO_COMPLETADO
+accion.invocar   EQU 0
+accion.destino   EQU 8
+accion.siguiente EQU 16
+accion.SIZE      EQU 24
 
 ; Variables globales de sólo lectura
 section .rodata
 
-; Marca el ejercicio 1 como hecho (`true`) o pendiente (`false`).
+; Marca el ejercicio 1 como hecho (true) o pendiente (false).
 ;
 ; Funciones a implementar:
 ;   - hay_accion_que_toque
 global EJERCICIO_1_HECHO
-EJERCICIO_1_HECHO: db FALSE
+EJERCICIO_1_HECHO: db TRUE
 
-; Marca el ejercicio 2 como hecho (`true`) o pendiente (`false`).
+; Marca el ejercicio 2 como hecho (true) o pendiente (false).
 ;
 ; Funciones a implementar:
 ;   - invocar_acciones
 global EJERCICIO_2_HECHO
-EJERCICIO_2_HECHO: db FALSE
+EJERCICIO_2_HECHO: db TRUE
 
-; Marca el ejercicio 3 como hecho (`true`) o pendiente (`false`).
+; Marca el ejercicio 3 como hecho (true) o pendiente (false).
 ;
 ; Funciones a implementar:
 ;   - contar_cartas
 global EJERCICIO_3_HECHO
-EJERCICIO_3_HECHO: db FALSE
+EJERCICIO_3_HECHO: db TRUE
 
 section .text
 
@@ -68,22 +68,55 @@ section .text
 ;
 ; El resultado es un valor booleano, la representación de los booleanos de C es
 ; la siguiente:
-;   - El valor `0` es `false`
-;   - Cualquier otro valor es `true`
+;   - El valor 0 es false
+;   - Cualquier otro valor es true
 ;
-; ```c
+; c
 ; bool hay_accion_que_toque(accion_t* accion, char* nombre);
-; ```
+; 
 global hay_accion_que_toque
 hay_accion_que_toque:
 	; Te recomendamos llenar una tablita acá con cada parámetro y su
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
-	; r/m64 = accion_t*  accion
-	; r/m64 = char*      nombre
-	xor rax, rax
-	ret
+	; rdi = accion_t*  accion
+	; rsi = char*      nombre
+	push rbp
+	mov rbp, rsp
+	push r12
+	push r13
+	push r14 ; prologo
+	sub rsp, 8 ; alineacion a pila
+	xor rax, rax ; rax = 0
+	mov r12, rdi ; r12 = puntero a accion
+	mov r13, rsi ; r13 = puntero a nombre
+	mov r14, FALSE ; r14 = FALSE
+
+	.ciclo: ; while
+		cmp r12,0 ; puntero a null, termino el ciclo
+		je .fin
+		mov rdi, [r12 + accion.destino] ; rdi = puntero a carta destino
+		add rdi, carta.nombre ; rdu = puntero al nombre de la carta destino
+		mov rsi, r13 ; rsi = puntero al nombre de carta buscado
+		call strcmp
+		cmp eax, 0 ; son iguales
+		jne .siguiente
+		mov r14, TRUE
+		jmp .fin
+
+	.siguiente:
+		mov r12, [r12 + accion.siguiente] ; r12 = siguiente accion
+		jmp .ciclo
+
+	.fin:
+		mov rax, r14 ; devuelvo el resultado en rax
+		add rsp, 8
+		pop r14 ; epilogo
+		pop r13
+		pop r12
+		pop rbp
+		ret
 
 ; Invoca las acciones que fueron encoladas en la secuencia proporcionada en el
 ; primer parámetro.
@@ -95,28 +128,57 @@ hay_accion_que_toque:
 ; se debe marcar ésta como fuera de juego.
 ;
 ; Las funciones que implementan acciones de juego tienen la siguiente firma:
-; ```c
+; c
 ; void mi_accion(tablero_t* tablero, carta_t* carta);
-; ```
+; 
 ; - El tablero a utilizar es el pasado como parámetro
-; - La carta a utilizar es la carta destino de la acción (`accion->destino`)
+; - La carta a utilizar es la carta destino de la acción (accion->destino)
 ;
 ; Las acciones se deben invocar en el orden natural de la secuencia (primero la
 ; primera acción, segundo la segunda acción, etc). Las acciones asumen este
 ; orden de ejecución.
 ;
-; ```c
+; c
 ; void invocar_acciones(accion_t* accion, tablero_t* tablero);
-; ```
+; 
 global invocar_acciones
 invocar_acciones:
 	; Te recomendamos llenar una tablita acá con cada parámetro y su
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
-	; r/m64 = accion_t*  accion
-	; r/m64 = tablero_t* tablero
-	ret
+	; rdi = accion_t*  accion
+	; rsi = tablero_t* tablero
+	push rbp
+	mov rbp, rsp 
+	push r12
+	push r13 ; prologo
+	mov r12, rdi ; lista de acciones
+	mov r13, rsi ; tablero
+
+	.ciclo:
+		cmp r12, 0
+		je .fin
+		mov r8, [r12 + accion.destino] ; r8 = puntero a carta
+		cmp BYTE [r8 + carta.en_juego], FALSE ; veo si la carta esta en juego
+		je .siguiente
+		mov rdi, r13 ; puntero al tablero
+		mov rsi, r8 ; puntero a la carta actual
+		call [r12 + accion.invocar] ; llamo a la funcion
+		mov r8, [r12 + accion.destino] ; r8 = puntero a la carta destino
+		cmp WORD [r8 + carta.vida], FALSE ; veo si la carta sigue teniendo vida
+		jne .siguiente ; esta viva
+		mov BYTE [r8 + carta.en_juego], FALSE ; deja de estar en juego
+	
+	.siguiente:
+		mov r12, [r12 + accion.siguiente] ; siguiente accion
+		jmp invocar_acciones.ciclo ; vuelvo al ciclo
+
+	.fin:
+		pop r13 ; epilogo
+		pop r12
+		pop rbp
+		ret
 
 ; Cuenta la cantidad de cartas rojas y azules en el tablero.
 ;
@@ -134,16 +196,47 @@ invocar_acciones:
 ; El resultado debe ser escrito en las posiciones de memoria proporcionadas
 ; como parámetro.
 ;
-; ```c
+; c
 ; void contar_cartas(tablero_t* tablero, uint32_t* cant_rojas, uint32_t* cant_azules);
-; ```
+; 
 global contar_cartas
 contar_cartas:
 	; Te recomendamos llenar una tablita acá con cada parámetro y su
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
-	; r/m64 = tablero_t* tablero
-	; r/m64 = uint32_t*  cant_rojas
-	; r/m64 = uint32_t*  cant_azules
-	ret
+	; rdi = tablero_t* tablero
+	; rsi = uint32_t*  cant_rojas
+	; rdx = uint32_t*  cant_azules
+	push rbp
+	mov rbp, rsp ; prologo
+	lea rdi, [rdi + tablero.campo] ; puntero al primer casillero
+	mov DWORD [rsi], 0 ; *cant_rojas = 0
+	mov DWORD [rdx], 0 ; *cant_azules = 0
+	xor r9, r9 ; r9 = 0
+
+	.ciclo:
+		cmp r9, tablero.ALTO*tablero.ANCHO ; veo si recorri todos los casilleros
+		je .fin
+		mov r10, [rdi + r9*8] ; r10 = carta en casilla, 8 porque son punteros
+		cmp r10, 0 ; comparo r10 con null
+		je .siguiente ; si es null salto al siguiente
+
+	.jugadorAzul:
+		cmp BYTE [r10 + carta.jugador], JUGADOR_AZUL ; veo si la carta es del jugador azul
+		jne .jugadorRojo
+		inc DWORD [rdx] ; incremento el contador de cartas azules
+		jmp .siguiente
+
+	.jugadorRojo:
+		cmp BYTE [r10 + carta.jugador], JUGADOR_ROJO ; veo si la carta es del jugador rojo
+		jne .siguiente
+		inc DWORD [rsi] ; incremento el contador de cartas rojas
+
+	.siguiente:
+		inc r9
+		jmp .ciclo
+
+	.fin:
+		pop rbp ; epilogo
+		ret
